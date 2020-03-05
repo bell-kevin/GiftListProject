6c687c6590bcacefdcb0d1c64ee406040a396ccb
JCBoyd <jcboyd@pm.me>
2020-03-05 08:39:52 -0700

    Create shell, commit, and push to remote

---
+++ src/giftlist/GiftList.java
@@ -0,0 +1,19 @@
+package giftlist;
+
+/******************************************************************************
+ * GiftList.java 
+ * Programmer: @author jcboyd
+ * Version: 1.0
+ * Course: SDEV 2210
+ *
+ * This program creates and displays a simple gift list using an array
+ *****************************************************************************/
+
+public class GiftList {
+
+    public static void main(String[] args) {
+
+
+    } //end main
+    
+} //end class GiftList
---
+++ src/giftlist/Gifts.java
@@ -0,0 +1,15 @@
+package giftlist;
+
+/******************************************************************************
+ * Gifts.java 
+ * Programmer: @author jcboyd
+ * 
+ * This class is associated with GiftList.java
+ *****************************************************************************/
+
+public class Gifts {
+    
+    
+    
+    
+} //end class Gifts
